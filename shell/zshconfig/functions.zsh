3fa41ed225cedbe9807f8a6db92ba6b07be01e9c
cam-ball <cameronb@pvep.com>
2022-11-13 20:58:27 -0500

    Add bootstrap shortcut & fzf history searcher

--- shell/zshconfig/functions.zsh
+++ shell/zshconfig/functions.zsh
@@ -12,6 +12,17 @@ function pretty_csv {
   column -t -s, -n "$@" | less -F -S -X -K
 }
 
+function bootstrap_dev {
+  bundle exec rails db:drop db:create db:migrate
+  bundle exec rails sync
+  bundle exec rails bootstrap
+}
+
+# ripped from https://github.com/junegunn/fzf/wiki/Examples#command-history
+function fh {
+  print -z $(history | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
+}
+
 # add local binfile to path for script storage
 if [ -d "$HOME/bin" ] ; then
   export PATH="$HOME/bin:$PATH"
